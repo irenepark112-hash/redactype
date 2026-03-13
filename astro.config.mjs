cd3fa389e038c4399c8bd8e264c8c69e58f0670a
irenepark112-hash <irenepark112@gmail.com>
2026-03-13 12:04:15 -0400

    other chnages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+const isGitHubPages = !!process.env.GITHUB_ACTIONS;
+
 export default defineConfig({
-  site: 'https://irenepark112-hash.github.io',
-  base: '/redactype/',
+  site: isGitHubPages
+    ? 'https://irenepark112-hash.github.io'
+    : 'http://localhost:4321',
+  base: isGitHubPages ? '/redactype/' : '/',
 });
