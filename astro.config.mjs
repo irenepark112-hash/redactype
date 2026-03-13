e2fb9a3eb7d1da661f7ffb1cf460ae97f783693e
irenepark112-hash <irenepark112@gmail.com>
2026-03-13 11:53:57 -0400

    scss fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://irenepark112-hash.github.io',
-  base: '/redactype',
+  base: '/redactype/',
 });
